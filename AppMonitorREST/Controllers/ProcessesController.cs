96c17722a1252dc0126ff5be2743c53555d88b1a
lordspinach <49073719+lordspinach@users.noreply.github.com>
2021-03-25 22:56:12 +0300

    Add controller with POST method that allow to add processes to database

---
+++ AppMonitorREST/Controllers/ProcessesController.cs
@@ -0,0 +1,69 @@
+﻿using AppMonitorREST.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AppMonitorREST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProcessesController : ControllerBase
+    {
+        readonly private ApplicationContext db;
+
+        public ProcessesController(ApplicationContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return db.Processes.Select(p => p.ProcessName).ToArray();
+        }
+        
+        [HttpPost]
+        [Route("new")]
+        public void New([FromBody] ClientData model)
+        {
+            var pc = db.PCs.Where(p => p.PCName == model.PCName);
+            if (pc.Any())
+            {
+                foreach (var proc in model.Processes)
+                {
+                    db.Processes.Add(new Process()
+                    {
+                        WindowTitle = proc.WindowTitle,
+                        ProcessName = proc.ProcessName,
+                        LaunchTime = proc.LaunchTime,
+                        Client = pc.First()
+                    });
+                }
+            }
+            else
+            {
+                var client = new ClientData()
+                {
+                    PCName = model.PCName
+                };
+
+                db.PCs.Add(client);
+
+                foreach (var proc in model.Processes)
+                {
+                    db.Processes.Add(new Process()
+                    {
+                        WindowTitle = proc.WindowTitle,
+                        ProcessName = proc.ProcessName,
+                        LaunchTime = proc.LaunchTime,
+                        Client = client
+                    });
+                }
+            }
+            db.SaveChanges();
+        }
+    }
+}
