10b0bc8f964b907081ff59e0d06105afaddddf61
lordspinach <49073719+lordspinach@users.noreply.github.com>
2021-03-25 23:24:46 +0300

    Configure swagger. Added POST method description

--- AppMonitorREST/Controllers/ProcessesController.cs
+++ AppMonitorREST/Controllers/ProcessesController.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace AppMonitorREST.Controllers
 {
+    [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
     public class ProcessesController : ControllerBase
@@ -19,12 +20,43 @@ namespace AppMonitorREST.Controllers
             this.db = db;
         }
 
+        /// <summary>
+        /// Shows all processes in DB
+        /// </summary>
+        /// <returns> IEnumerable/<string/> </returns>
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return db.Processes.Select(p => p.ProcessName).ToArray();
         }
-        
+
+        /// <summary>
+        /// Add 
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     POST /api/Processes/new
+        ///     {
+        ///      "pcName": "MyPC",
+        ///      "processes": [
+        ///         {
+        ///           "WindowTitle": "GOOGLE CHROME",
+        ///           "ProcessName": "chrome",
+        ///           "launchTime": "2021-03-12T21:43:21"
+        ///         },
+        ///         {
+        ///           "WindowTitle": "AppMonitorREST",
+        ///           "ProcessName": "vs_studio",
+        ///           "launchTime": "2021-03-12T18:12:03"
+        ///         }
+        ///       ]
+        ///     }
+        ///     
+        /// </remarks>
+        /// <param name="model"></param>
+        /// <response code="200">Processes added succesefully</response>
+        /// [ProducesResponseType(StatusCodes.Status200Created)]
         [HttpPost]
         [Route("new")]
         public void New([FromBody] ClientData model)
--- AppMonitorREST/Startup.cs
+++ AppMonitorREST/Startup.cs
@@ -5,12 +5,15 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
 
 namespace AppMonitorREST
 {
@@ -28,7 +31,24 @@ namespace AppMonitorREST
             string connection = Configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
             services.AddControllers();
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "MonitorApp API",
+                    Description = "Simple API for MonitorApp",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Konstantin Gamayunov",
+                        Email = "ksgamayunov@gmail.com",
+                        Url = new Uri("https://vk.com/lifeisntleaf")
+                    }
+                });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         public void Configure(IApplicationBuilder app)
