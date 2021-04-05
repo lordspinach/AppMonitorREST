a3e61c04097bed1f91edc352cb57cfe010c9e668
lordspinach <49073719+lordspinach@users.noreply.github.com>
2021-04-05 13:37:07 +0300

    Added AuthController and Login model

---
+++ AppMonitorREST/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using AppMonitorREST.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AppMonitorREST.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AuthController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody]Login requestBody)
+        {
+            var user = await _userManager.FindByNameAsync(requestBody.UserName);
+            if (user == null)
+                return NotFound();
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, requestBody.Password, false);
+
+            if (result.Succeeded)
+            {
+                return Ok(new 
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName,
+                    token = "temp"
+                });
+            }
+            return Unauthorized();
+        }
+    }
+}
---
+++ AppMonitorREST/Models/Login.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMonitorREST.Models
+{
+    public class Login
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+        public Login()
+        {
+
+        }
+    }
+}
